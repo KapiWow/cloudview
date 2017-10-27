455c7685d80075a1c15ecf89032c37e4041c537f
KapiWow <5158887@mail.ru>
2017-10-27 15:37:46 +0300

    chart changes

--- chart.js
+++ chart.js
@@ -14,17 +14,16 @@ function drawChart() {
     data.addColumn('string', 'Topping');
     data.addColumn('number', 'Slices');
     data.addRows([
-        ['Mushrooms', 3],
-        ['Onions', 1],
-        ['Olives', 1],
-        ['Zucchini', 1],
-        ['Pepperoni', 2]
+        ['Europe', 46],
+        ['North America', 340],
+        ['Asia', 200],
+        ['Africa', 18]
     ]);
 
     // Set chart options
-    var options = {'title':'How Much Pizza I Ate Last Night',
-        'width':400,
-        'height':300};
+    var options = {'title':'Country',
+        'width':600,
+        'height':400};
 
     // Instantiate and draw our chart, passing in some options.
     var chart = new google.visualization.PieChart(document.getElementById('chart_div'));
