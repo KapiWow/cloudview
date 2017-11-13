6f2020081e543d5f7347d1a86af105efb4957b33
KapiWow <5158887@mail.ru>
2017-11-13 12:31:55 +0300

    test send JSON

--- node/index.js
+++ node/index.js
@@ -1,32 +1,34 @@
 var http = require('http');
-var static = require('node-static');
-// var pg = require('pg');
-var file = new static.Server('./..');
+var nodeStatic = require('node-static');
+var file = new nodeStatic.Server('./..');
+
 //postgres
-const { Client } = require('pg')
-const connectionString = 'postgresql://cloud_user:1349@localhost:5432/cloud'
+const {Client} = require('pg');
+const connectionString = 'postgresql://cloud_user:1349@localhost:5432/cloud';
 
 const client = new Client({
-    connectionString: connectionString,
-})
-client.connect()
+    connectionString: connectionString
+});
+client.connect();
 
-client.query('SELECT * FROM usage', (err, res) => {
-    console.log(res.rows[0].datatime)
-    client.end()
-})
 
-// function accept(req, res) {
-// if (req.url = '/vote') {
 
-// } else {
-// 	file.serve(req, res);
-// }
-// }
+function accept(req, res) {
+    if (req.url === '/getInfo') {
+        // console.log('getInfoServer');
+        client.query('SELECT * FROM usage', (err, resDB) => {
+            // console.log(resDB.rows[0].datatime)
+            res.end(JSON.stringify(resDB.rows[0]));
+            // console.log(typeof resDB.rows[0].datatime);
+            // console.log(typeof resDB.rows[0].cpu);
+            // console.log(typeof Client);
+        })
+    } else {
+        file.serve(req, res);
+    }
+}
 
 //create server 
-http.createServer(function(req, res) {
-	file.serve(req, res);
-}).listen(8080);
+http.createServer(accept).listen(8080);
 
 console.log('Server running on port 8080');
