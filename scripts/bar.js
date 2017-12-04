241e86b77796d91aa382cd5a2093ee76ccbca218
KapiWow <5158887@mail.ru>
2017-12-04 23:23:11 +0300

    remove js

---
+++ scripts/bar.js
@@ -0,0 +1,56 @@
+function clearBar() {
+    userBar.setAttribute('class', "w3-bar-item w3-button w3-padding");
+    serverBar.setAttribute('class', "w3-bar-item w3-button w3-padding");
+    statBar.setAttribute('class', "w3-bar-item w3-button w3-padding");
+    monitorBar.setAttribute('class', "w3-bar-item w3-button w3-padding");
+}
+
+$(document).ready(function () {
+    $.ajax({
+        url: "user.html",
+        cache: false,
+        success: function (html) {
+            $("#content").html(html);
+        }
+    });
+});
+
+function barClick(sender, barID) {
+    urlName = "";
+    bar = userBar;
+    switch (barID) {
+        case 1:
+            urlName = "user.html";
+            bar = userBar;
+            break;
+        case 2:
+            urlName = "server.html";
+            bar = serverBar;
+            break;
+        case 3:
+            urlName = "stat.html";
+            bar = statBar;
+            break;
+        case 4:
+            urlName = "history.html";
+            bar = historyBar;
+            break;
+        case 5:
+            urlName = "monitor.html";
+            bar = monitorBar;
+            break;
+        default:
+            break;
+    }
+    $(document).ready(function () {
+        $.ajax({
+            url: urlName,
+            cache: false,
+            success: function (html) {
+                $("#content").html(html);
+            }
+        });
+    });
+    clearBar();
+    bar.setAttribute('class', "w3-bar-item w3-button w3-padding w3-blue");
+}
